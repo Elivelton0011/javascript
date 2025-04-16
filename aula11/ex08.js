5d20c42fcf266f6621913c70a589c6c79f1c46b5
Elivelton Chaves <elicomercial0011@gmail.com>
2025-04-16 18:26:14 -0300

    modulo D

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var km = 60
+if (km > 60) {
+    console.log(`vocé está á ${km} isso está acima da velocidade, MULTADO!`)
+} else {
+    console.log(`Tenha um otimo dia`)
+}
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+
+if (pais == 'Brasil') {
+    console.log(`Se vocé nasceu no ${pais} você é Brasileiro!`)
+} else {
+    console.log (`Se você nasceu em ${pais} você é estrangeiro!`)
+}
